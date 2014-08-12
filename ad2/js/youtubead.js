be42d5c00f1e954862d468d0e494933621c3bc53
junlin <junlin@fu101.com>
2014-08-12 11:29:46 +0800

    test ad2

---
+++ ad2/js/youtubead.js
@@ -0,0 +1,45 @@
+function loadYtplayer(){
+ var tag = document.createElement('script');
+ tag.src = "http://www.youtube.com/iframe_api";
+ var firstScriptTag = document.getElementsByTagName('script')[0];
+ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+var player;
+var key =  videoIds = document.URL.split('#');
+function onYouTubeIframeAPIReady() {
+  var youtubekey = key[1];
+  player = new YT.Player('player', { 
+    height: '315',
+    width: '980',
+    videoId: youtubekey,
+    playerVars: {rel: 0, showinfo: 0,autoplay:1,controls:0},
+    events: {
+      'onStateChange':onPlayerStateChange 
+    }
+  });
+ }
+
+function onPlayerStateChange(event) {
+  if(event.data == YT.PlayerState.ENDED) {
+    playtime = player.getCurrentTime();
+    player.seekTo(0);
+  }
+}
+
+function ytbclose(){
+  playtime = player.getCurrentTime();
+  player.pauseVideo();
+  player.seekTo(0);
+  $("#youtube").hide();
+}
+
+function playing(){
+   player.seekTo(0);
+   player.playVideo();
+   $("#youtube").show();
+}
+
+$(document).ready(function(){
+   loadYtplayer();
+});
